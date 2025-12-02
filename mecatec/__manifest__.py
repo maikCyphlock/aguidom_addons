5e1bff239201f25053e6db8a19b94b3554bb0522
maikol aguilar <66704744+maikCyphlock@users.noreply.github.com>
2025-12-02 19:35:18 -0400

    test

--- mecatec/__manifest__.py
+++ mecatec/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mecatec Management',
-    'version': '1.0',
+    'version': '1.0.0',
     'category': 'Services',
     'summary': 'Inventory, Maintenance, and Appointment Management for Mecatec',
     'description': """
